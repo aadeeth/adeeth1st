0762fb222989e374aa00aadb0831af282f4d353d
Adeeth Aadarash <aaadarash@aaadarash-mbp.jnpr.net>
2021-02-18 10:19:12 +0530

    commit

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+   int a,b=0,c[100];
+   cin>>a;
+   for(int i=0;i<a;i++)
+    {   cin>>c[i];
+        b=b+c[i];
+    }
+  cout<<b;
+return 0;
+}
+   
